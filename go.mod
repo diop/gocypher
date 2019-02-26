d56ff7ce8b942293e6a4d10b477312c6d7816b80
diop <fodesdiop@gmail.com>
2019-02-26 00:35:25 -0800

    hide credentials and connect to BlockCypher api

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/diop/gocypher
 
 go 1.12
+
+require (
+	github.com/blockcypher/gobcy v1.3.1
+	github.com/btcsuite/btcd v0.0.0-20190213025234-306aecffea32 // indirect
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/blockcypher/gobcy"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Go utility libraries for BlockCypher")
+	// Initialize godotenv for reading secrets stored in .env files.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	btc := gobcy.API{os.Getenv("TOKEN"), "btc", "main"}
+	chain, err := btc.GetChain()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("%+v\n", chain)
 }
